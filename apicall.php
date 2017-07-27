493403c22a4cd8a5cccfa9deb758fb4f7daaf494
hhan69 <30454830+hhan69@users.noreply.github.com>
2017-07-27 00:36:40 -0400

    Create apicall.php

---
+++ apicall.php
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2017 ChemADVISOR, Inc. All rights reserved.
+ * Licensed under The MIT License (MIT)
+ * https://opensource.org/licenses/MIT
+ */
+
+<?php
+
+// set base address
+$baseAddress = "https://sandbox.chemadvisor.io/chem/rest/v2/";
+
+// set api
+$api = "lists";
+
+// set query parameters:q,limit,offset
+$q      = '{"tags.tag.name":"Government Inventory Lists"}';
+$limit  = 10;
+$offset = 0;
+
+$getdata = http_build_query(array(
+    'q' => $q,
+    'limit' => $limit,
+    'offset' => $offset
+));
+
+$opts = array(
+    'http' => array(
+        'method' => "GET",
+        'header' => 
+        // set app_key header
+            "app_key:your_app_key\r\n" . 
+		// set app_id header
+            "app_id:your_app_id\r\n" .
+        // set accept header
+            "accept:application/json\r\n"
+    )
+);
+
+$context = stream_context_create($opts);
+$file = file_get_contents($baseAddress . $api . '?' . $getdata, false, $context);
+print_r($file);
+?>
